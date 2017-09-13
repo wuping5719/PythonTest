43c3d6c4241dfb7e91ee0f60b3278c99b545361c
PingWu <wp571988@163.com>
2017-09-13 23:00:09 +0800

    Create test21.py

---
+++ src/test/test21.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+'''
+Created on 2017 年 9 月  13 日
+
+@author: Nick
+'''
+
+'''
+题目：利用递归函数调用方式，将所输入的5个字符，以相反顺序打印出来。
+'''
+
+s = input('Input a string:')
+l = list(s)
+l.reverse()
+print(l);
