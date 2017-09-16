e26c2d694601ca4bbaa3712979dfa51372d4cf4e
PingWu <wp571988@163.com>
2017-09-16 15:46:50 +0800

    Create test42.py

---
+++ src/test/test42.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+'''
+Created on 2017 年 9 月  16 日
+
+@author: Nick
+'''
+
+'''
+题目：输出一个随机数。
+程序分析：使用 random 模块。
+'''
+
+import random
+ 
+#生成 10 到 20 之间的随机数
+print (random.uniform(10, 20))
