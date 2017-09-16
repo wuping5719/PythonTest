d61ce87281b57074342a6574059f3ff2f9d52a82
PingWu <wp571988@163.com>
2017-09-16 15:33:57 +0800

    Create test37.py

---
+++ src/test/test37.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+'''
+Created on 2017 年 9 月  16 日
+
+@author: Nick
+'''
+
+'''
+题目：统计 1 到 100 之和。
+'''
+
+tmp = 0
+for i in range(1,101):
+    tmp += i
+print ('The sum is %d' % tmp)
