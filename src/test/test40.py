28a918a630cddb4d547632d35ad22d87107972de
PingWu <wp571988@163.com>
2017-09-16 15:43:24 +0800

    Create test40.py

---
+++ src/test/test40.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+'''
+Created on 2017 年 9 月  16 日
+
+@author: Nick
+'''
+
+'''
+题目：数字比较。
+'''
+
+if __name__ == '__main__':
+    i = 10
+    j = 20
+    if i > j:
+        print ('%d 大于 %d' % (i,j))
+    elif i == j:
+        print ('%d 等于 %d' % (i,j))
+    elif i < j:
+        print ('%d 小于 %d' % (i,j))
+    else:
+        print ('未知')
