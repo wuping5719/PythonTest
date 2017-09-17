ec7ebd9e2c01ec65ee26d6b2f0f02f1644c4812c
PingWu <wp571988@163.com>
2017-09-17 21:59:23 +0800

    Create test53.py

---
+++ src/test/test53.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+'''
+Created on 2017 年 9 月  17 日
+
+@author: Nick
+'''
+
+'''
+题目：输入3个数a,b,c，按大小顺序输出。　
+'''
+
+if __name__ == '__main__':
+    n1 = int(input('n1 = :\n'))
+    n2 = int(input('n2 = :\n'))
+    n3 = int(input('n3 = :\n'))
+ 
+    def swap(p1,p2):
+        return p2,p1
+ 
+    if n1 > n2 : n1,n2 = swap(n1,n2)
+    if n1 > n3 : n1,n3 = swap(n1,n3)
+    if n2 > n3 : n2,n3 = swap(n2,n3)
+ 
+    print (n1,n2,n3)
