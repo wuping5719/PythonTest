1204617dc5cec8362b00b3c82422d7d63ba78386
PingWu <wp571988@163.com>
2017-09-17 15:25:04 +0800

    Create test48.py

---
+++ src/test/test48.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+'''
+Created on 2017 年 9 月  17 日
+
+@author: Nick
+'''
+
+'''
+题目：画图，学用circle画圆形.
+'''
+
+if __name__ == '__main__':
+    import turtle
+    pen=turtle.Turtle()
+    pen.color("yellow")
+    pen.width(5)
+    pen.shape("turtle")
+    pen.speed(1)
+    pen.circle(100)
