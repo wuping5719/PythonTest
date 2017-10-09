75863e61352daf6a7210c9bba2d7d8b5005965ed
PingWu <wp571988@163.com>
2017-10-09 21:08:04 +0800

    Create test59.py

---
+++ src/test/test59.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+'''
+Created on 2017 年 10 月  9 日
+
+@author: Nick
+'''
+
+'''
+题目：创建一个链表。
+'''
+
+if __name__ == '__main__':
+    ptr = []
+    for i in range(5):
+        num = int(input('please input a number:\n'))
+        ptr.append(num)
+    print (ptr)
