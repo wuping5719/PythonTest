d61a7e6f7dec6665148239df97c58effb5a845ce
PingWu <wp571988@163.com>
2017-10-09 21:00:09 +0800

    Create test57.py

---
+++ src/test/test57.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+'''
+Created on 2017 年 10 月  9 日
+
+@author: Nick
+'''
+
+'''
+题目：写一个函数，求一个字符串的长度，在main函数中输入字符串，并输出其长度。
+'''
+
+if __name__ == '__main__':
+    s = input('please input a string:\n')
+    print ('the string has %d characters.' % len(s))
