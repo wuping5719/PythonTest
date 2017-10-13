ea218e13c3fedf7a9bd72a1184cc5dcc04c61e4d
PingWu <wp571988@163.com>
2017-10-13 21:58:56 +0800

    Create test61.py

---
+++ src/test/test61.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+'''
+Created on 2017 年 10 月  13 日
+
+@author: Nick
+'''
+
+'''
+题目：列表排序及连接。
+程序分析：排序可使用  sort()方法，连接可以使用 +号或  extend()方法。
+'''
+
+if __name__ == '__main__':
+    a = [1,3,2]
+    b = [3,4,5]
+    a.sort()     # 对列表 a 进行排序
+    print (a)
+ 
+    # 连接列表 a与 b
+    print (a+b)
+    
+    # 连接列表 a与 b
+    a.extend(b)
+    print (a)
