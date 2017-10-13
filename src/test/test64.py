22070182afbd1c3c15675e78699bd95567274588
PingWu <wp571988@163.com>
2017-10-13 22:17:57 +0800

    Create test64.py

---
+++ src/test/test64.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+'''
+Created on 2017 年 10 月  13 日
+
+@author: Nick
+'''
+
+'''
+题目：循环输出列表.
+'''
+
+if __name__ == '__main__':
+    s = ["man","woman","girl","boy","sister"]
+    for i in range(len(s)):
+        print (s[i])
