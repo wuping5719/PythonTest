d8e4cbdcbb8b6ecef0c1ade8b7ae34f893a7a672
PingWu <wp571988@163.com>
2017-10-14 21:14:42 +0800

    Create test73.py

---
+++ src/test/test73.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+'''
+Created on 2017 年 10 月  14 日
+
+@author: Nick
+'''
+
+'''
+题目：两个字符串连接程序。
+'''
+
+if __name__ == '__main__':
+    a = "acegikm"
+    b = "bdfhjlnpq"
+
+    # 连接字符串
+    c = a + b
+    print (c)
