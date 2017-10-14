264e897253ebf8f4488ef3dc67b38d17f74aa40d
PingWu <wp571988@163.com>
2017-10-14 21:09:20 +0800

    Create test71.py

---
+++ src/test/test71.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+'''
+Created on 2017 年 10 月  14 日
+
+@author: Nick
+'''
+
+'''
+题目：连接字符串。
+'''
+
+delimiter = ','
+mylist = ['Brazil', 'Russia', 'India', 'China']
+print (delimiter.join(mylist))
