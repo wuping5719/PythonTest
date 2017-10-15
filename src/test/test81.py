17319fc2d0a06d3c56d3b373f037d34e23e6a82b
PingWu <wp571988@163.com>
2017-10-15 21:49:26 +0800

    Create test81.py

---
+++ src/test/test81.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+'''
+Created on 2017 年 10 月  15 日
+
+@author: Nick
+'''
+
+'''
+题目：计算字符串中子串出现的次数。
+'''
+
+if __name__ == '__main__':
+    str1 = input('请输入一个字符串:\n')
+    str2 = input('请输入一个子字符串:\n')
+    ncount = str1.count(str2)
+    print (ncount)
